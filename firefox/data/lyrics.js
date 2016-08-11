3587aed58ac94822babeb5f45d55a8df09ec8fbd
Jeremy Woertink <jeremywoertink@gmail.com>
2016-08-11 14:03:41 -0700

    updated firefox build

--- firefox/data/lyrics.js
+++ firefox/data/lyrics.js
@@ -1,50 +1,65 @@
-function getTextWidth(text) {
-  var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-  var context = canvas.getContext("2d");
-  context.font = "Helvetica 18px";
-  var metrics = context.measureText(text);
-  return metrics.width;
-}
+(function() {
+  var container, getLongestLine, getTextWidth, i, lyricBlocks, node, p, text, textNode, titles, width;
 
-function getLongestLine(textBlock) {
-  var lines = textBlock.split("\n");
-  var line = lines.sort(function(a, b) { return b.length - a.length; });
-  return line[0];
-}
+  getTextWidth = function(text) {
+    var canvas, context, metrics;
+    canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement('canvas'));
+    context = canvas.getContext('2d');
+    context.font = 'Helvetica 18px';
+    metrics = context.measureText(text);
+    return metrics.width;
+  };
 
-document.getElementById("material-app-bar").style.display = "none";
-document.querySelector("#main-content-container .header.track").style.display = "none";
+  getLongestLine = function(textBlock) {
+    var line, lines;
+    lines = textBlock.split('\n');
+    line = lines.sort(function(a, b) {
+      return b.length - a.length;
+    });
+    return line[0];
+  };
 
-var titles = document.querySelectorAll("#main-content-container .title");
+  document.getElementById('material-app-bar').style.display = 'none';
 
-for(var i=0; i < titles.length; i++) {
-  titles[i].style.display = "none";
-}
+  document.querySelector('#main-content-container .header.track').style.display = 'none';
 
-var container = document.querySelector("#main-content-container .content-container.lyrics");
-container.style.margin = "0px";
+  titles = document.querySelectorAll('#main-content-container .title');
 
-var lyricBlocks = document.querySelectorAll("#main-content-container .content-container.lyrics p");
+  i = 0;
 
-for(var i=0; i < lyricBlocks.length; i++) {
-  var p = lyricBlocks[i];
-  var text = getLongestLine(p.innerText);
-  var width = getTextWidth(text);
-  p.style.float = "left";
-  p.style.width = width + "px";
-  p.style.marginRight = "10px";
-  p.style.padding = "10px";
-  p.style.border = "1px solid #eeeeee";
-  p.style.position = "relative";
+  while (i < titles.length) {
+    titles[i].style.display = 'none';
+    i++;
+  }
 
-  var node = document.createElement("SPAN");
-  var textNode = document.createTextNode(i + 1);
-  node.appendChild(textNode);
-  node.style.position = "absolute";
-  node.style.left = "1px";
-  node.style.top = "1px";
-  node.style.color = "black";
-  node.style.fontSize = "8px";
-  p.appendChild(node);
-}
+  container = document.querySelector('#main-content-container .content-container.lyrics');
 
+  container.style.margin = '0px';
+
+  lyricBlocks = document.querySelectorAll('#main-content-container .content-container.lyrics p');
+
+  i = 0;
+
+  while (i < lyricBlocks.length) {
+    p = lyricBlocks[i];
+    text = getLongestLine(p.innerText);
+    width = getTextWidth(text);
+    p.style.float = 'left';
+    p.style.width = width + 'px';
+    p.style.marginRight = '10px';
+    p.style.padding = '10px';
+    p.style.border = '1px solid #eeeeee';
+    p.style.position = 'relative';
+    node = document.createElement('SPAN');
+    textNode = document.createTextNode(i + 1);
+    node.appendChild(textNode);
+    node.style.position = 'absolute';
+    node.style.left = '1px';
+    node.style.top = '1px';
+    node.style.color = 'black';
+    node.style.fontSize = '8px';
+    p.appendChild(node);
+    i++;
+  }
+
+}).call(this);
